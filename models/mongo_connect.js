7443b78a01c212f0b24a0e9ef490006481186e90
Simba <nikolayhouse128@gmail.com>
2017-04-21 03:27:44 +0800

    Change db path

--- models/mongo_connect.js
+++ models/mongo_connect.js
@@ -2,4 +2,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/school-db');
+//mongoose.connect('mongodb://localhost:27017/school-db');
+const username = 'snowsea';
+const password = 'snowsea';
+mongoose.connect('mongodb://' + username + ':' + password + '@ds111461.mlab.com:11461/heroku_m4821xkr');
