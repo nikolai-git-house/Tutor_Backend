4a26f99a74f8adff502c46031d29fb71724e816b
Simba <nikolayhouse128@gmail.com>
2017-04-22 03:07:05 +0800

    fix db connection string

--- models/mongo_connect.js
+++ models/mongo_connect.js
@@ -5,4 +5,4 @@ mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/school-db');
 const username = 'snowsea';
 const password = '4a>#X[(V6~g8RvJ~u7';
-mongoose.connect('mongodb://' + username + ':' + password + 'ds115091-a0.mlab.com:15091,ds115091-a1.mlab.com:15091/heroku_ldk8l55q?replicaSet=rs-ds115091');
+mongoose.connect('mongodb://' + username + ':' + password + '@ds115091-a0.mlab.com:15091,ds115091-a1.mlab.com:15091/heroku_ldk8l55q?replicaSet=rs-ds115091');
