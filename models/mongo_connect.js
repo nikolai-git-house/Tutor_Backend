9ffe76aa2aabca3925cec8b249bfaecfce8d538f
Simba <nikolayhouse128@gmail.com>
2017-04-22 02:18:23 +0800

    change db information

--- models/mongo_connect.js
+++ models/mongo_connect.js
@@ -4,5 +4,5 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/school-db');
 const username = 'snowsea';
-const password = 'snowsea';
-mongoose.connect('mongodb://' + username + ':' + password + '@ds111461.mlab.com:11461/heroku_m4821xkr');
+const password = '4a>#X[(V6~g8RvJ~u7';
+mongoose.connect('mongodb://' + username + ':' + password + 'ds115091-a0.mlab.com:15091,ds115091-a1.mlab.com:15091/heroku_ldk8l55q?replicaSet=rs-ds115091');
